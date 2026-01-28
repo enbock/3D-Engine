4fd472b36e76c1b70e343737c76dbeaf5577b071
Endre Bock <dev@itbock.de>
2026-01-29 00:42:49 +0100

    Enhance lighting parameters and triangle definitions for improved scene rendering

--- Application/Scene/SceneBuilderService.cs
+++ Application/Scene/SceneBuilderService.cs
@@ -19,21 +19,21 @@ public class SceneBuilderService
         scene.Camera.Position = new Vector3(0, 3, 10);
         scene.Camera.Target = new Vector3(0, 1, 0);
 
-        scene.AddLight(LightEntity.CreateAmbient(Color.White, 0f));
+        scene.AddLight(LightEntity.CreateAmbient(Color.White, 0.02f));
         scene.AddLight(LightEntity.CreateDirectional(new Vector3(0.5f, -1.0f, 0.3f), Color.White, 0.5f));
-        scene.AddLight(LightEntity.CreatePoint(new Vector3(-3, 4, 2), new Color(1.0f, 0.9f, 0.8f)));
+        scene.AddLight(LightEntity.CreatePoint(new Vector3(-3, 4, 2), new Color(1.0f, 0.9f, 0.8f), 2.0f));
 
         scene.AddTriangle(new TriangleEntity(
-            new Vector3(-2, 0, -1),
-            new Vector3(-1, 2, -1),
             new Vector3(-1, 0, -1),
+            new Vector3(-1, 2, -1),
+            new Vector3(-2, 0, -1),
             new Color(1.0f, 0.0f, 0.0f)
         ));
 
         scene.AddTriangle(new TriangleEntity(
-            new Vector3(0, 0, -0.5f),
-            new Vector3(0, 2, 0),
             new Vector3(0, 0, 0.5f),
+            new Vector3(0, 2, 0),
+            new Vector3(0, 0, -0.5f),
             new Color(0.0f, 1.0f, 0.0f)
         ));
 
